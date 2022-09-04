b009b1384c5f3eb058578435f7abb1e26be7ff1b
Vovan <plategi@yandex.ru>
2022-09-04 14:21:34 +0300

    from home

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ import classes from '../styles/error.module.scss';
 export default function ErrorPage() {
     return (
         <>
-            <h1 className={classes.error}>Error 404</h1>
+            <h1 className={classes.error}>Ошибка 404</h1>
             <p>Go to <Link href={'/'}><a>Home</a></Link></p>
         </>
     )
--- pages/500.js
+++ pages/500.js
@@ -4,7 +4,7 @@ import classes from '../styles/error.module.scss';
 export default function ErrorPage500() {
     return (
         <>
-            <h1 className={classes.error}>Error 500</h1>
+            <h1 className={classes.error}>Ошибка 500</h1>
             <p>Go to <Link href={'/'}><a>Home</a></Link></p>
         </>
     )
