960ad0b9ac62a62befbf30318e423753e2904f5e
Vovan <plategi@yandex.ru>
2022-09-04 22:02:17 +0300

    from home

--- pages/404.js
+++ pages/404.js
@@ -5,6 +5,7 @@ export default function ErrorPage() {
     return (
         <>
             <h1 className={classes.error}>Ошибка 404</h1>
+            <p>Страница не найдена</p>
             <p>Go to <Link href={'/'}><a>Home</a></Link></p>
         </>
     )
--- pages/500.js
+++ pages/500.js
@@ -5,6 +5,7 @@ export default function ErrorPage500() {
     return (
         <>
             <h1 className={classes.error}>Ошибка 500</h1>
+            <p>Ошибка сервера</p>
             <p>Go to <Link href={'/'}><a>Home</a></Link></p>
         </>
     )
