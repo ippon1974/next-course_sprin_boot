b80f28e2d9315feb91e175332964dae0d0928c29
Vovan <plategi@yandex.ru>
2022-09-07 22:04:40 +0300

    home

--- pages/form.js
+++ pages/form.js
@@ -1,9 +1,12 @@
 import {MainLayout} from "../components/MainLayout";
+import React, {useState} from 'react';
 
 export default function Form() {
+
     return (
         <MainLayout title={'Testing for Forms ...'}>
         <h1>Testing for Forms ...</h1>
+
         </MainLayout>
     )
 }
